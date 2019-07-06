55b37ce6639993ac770f21ec7b7c1767c642adf8
o.gavrilyuk <gavrilyuk.alex@gmail.com>
2019-07-06 23:53:28 +0300

    Complete H/W#6 - Refactored \ Error correction in sorting algorithm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         TreeSet set = new TreeSet<>(new OwnComparator());
         set.addAll(Arrays.asList(1, 7, 8, 6, 4, 3, 2, 9));
         System.out.println(set);
