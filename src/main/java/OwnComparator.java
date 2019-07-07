6decbee078ab28052e273a8f2399b42b8006004f
o.gavrilyuk <gavrilyuk.alex@gmail.com>
2019-07-07 20:45:44 +0300

    Complete H/W#6 - Refactored \ Code optimization

--- src/main/java/OwnComparator.java
+++ src/main/java/OwnComparator.java
@@ -7,19 +7,14 @@ public class OwnComparator implements Comparator<Integer> {
     public int compare(Integer o1, Integer o2) {
         if (o1 % 2 == 0) {
             if (o2 % 2 == 0) {
-                if (o1 < o2)
-                    return -1;
-                else
-                    return 1;
-            } else {
+                return o1.compareTo(o2);
+            }
+                 else {
                 return -1;
             }
         } else {
             if (o2 % 2 != 0) {
-                if (o1 < o2)
-                    return -1;
-                else
-                    return 1;
+                return o1.compareTo(o2);
             } else {
                 return 1;
             }
