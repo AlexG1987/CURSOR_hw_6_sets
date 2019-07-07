e60e5e959b4df70041bf236a66785673e01e7fdd
o.gavrilyuk <gavrilyuk.alex@gmail.com>
2019-07-07 21:27:15 +0300

    Fixed Raw Type enchecked error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        TreeSet set = new TreeSet<>(new OwnComparator());
-        set.addAll(Arrays.asList(1, 7, 8, 6, 4, 3, 2, 9));
+        TreeSet<Integer> set = new TreeSet<>(new OwnComparator());
+        set.addAll(Arrays.asList(1, 7, 6, 4, 3, 8, 2, 9));
         System.out.println(set);
     }
 }
