5e95e3b092ebf8ecc06591718998fbf89d1aa40e
o.gavrilyuk <gavrilyuk.alex@gmail.com>
2019-07-14 19:20:09 +0300

    Optimized code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 package main.java;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
 
--- src/main/java/OwnComparator.java
+++ src/main/java/OwnComparator.java
@@ -2,21 +2,14 @@ package main.java;
 
 import java.util.Comparator;
 
-public class OwnComparator implements Comparator<Integer> {
+class OwnComparator implements Comparator<Integer> {
 
     public int compare(Integer o1, Integer o2) {
-        if (o1 % 2 == 0) {
-            if (o2 % 2 == 0) {
-                return o1.compareTo(o2);
-            } else {
-                return -1;
-            }
-        } else {
-            if (o2 % 2 != 0) {
-                return o1.compareTo(o2);
-            } else {
-                return 1;
-            }
+        if ((o1 % 2) == 0) {
+            if (o2 % 2 == 0) return o1.compareTo(o2);
+            return -1;
         }
+        if (o2 % 2 != 0) return o1.compareTo(o2);
+        return 1;
     }
 }
